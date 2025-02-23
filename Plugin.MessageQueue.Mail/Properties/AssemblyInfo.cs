c6cef79393183e7a11c0c2ab2594b2de80da2160
DKorablin2 <danila.korablin@unity3d.com>
2025-02-24 00:28:59 +0300

    Unified plugins deployment

--- Plugin.MessageQueue.Mail/Properties/AssemblyInfo.cs
+++ Plugin.MessageQueue.Mail/Properties/AssemblyInfo.cs
@@ -1,7 +1,6 @@
 ﻿using System.Reflection;
 using System.Runtime.InteropServices;
 
-[assembly: ComVisible(false)]
 [assembly: Guid("53ee6768-4e0c-4391-a512-0a94b534f901")]
 [assembly: System.CLSCompliant(false)]
 
@@ -9,14 +8,6 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyMetadata("ProjectUrl", "https://github.com/DKorablin/Plugin.MessageQueue.Mail")]
 #else
 
-[assembly: AssemblyTitle("Plugin.MessageQueue.Mail")]
 [assembly: AssemblyDescription("Message queue plugin based on mail messages")]
-#if DEBUG
-[assembly: AssemblyConfiguration("Debug")]
-#else
-[assembly: AssemblyConfiguration("Release")]
-#endif
-[assembly: AssemblyCompany("Danila Korablin")]
-[assembly: AssemblyProduct("Plugin.MessageQueue.Mail")]
 [assembly: AssemblyCopyright("Copyright © Danila Korablin 2020")]
 #endif
